96f6e963ea5e194161274a60c2b3b72c6fbcd8bb
Nikita Sazonov <sazonovn2018@gmail.com>
2022-09-11 14:06:00 -0600

    c

---
+++ Project1_expressions_alert/main_js/JavaScript_File.js
@@ -0,0 +1,26 @@
+
+var Sent1 = "This is the beggining of the string ";
+var Sent2 = " This is the end of the string ";
+var result = (Sent1 + Sent2);
+
+document.write(result);
+
+
+
+document.write(" i am going to have a dinner with my frends today, " + "we will go to Matsuhisa" + " at 7pm.");// i have created text and concatenated with a few others
+
+
+var B = "Contcatenated" + "String ";
+document.write(B);
+
+
+var myFamily = "Myself ", Mom = "Wonder Woman ", Wife = "Lovely Humanbeing", Cat = "Prrrrr Sound";
+document.write(Cat)
+
+
+
+var Multiply = 4 * 8;
+document.write(Multiply);
+
+
+
